d71ca3ab07d5714581d7d69eb76e2f7abdd3e402
zafar ahmad <zafarswat9@gmail.com>
2021-09-22 01:09:16 -0700

    cart page

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,45 @@
-import React from 'react'
-
+import React,{useContext} from 'react'
+import { ListGroup, Row,Col,Form,Image } from 'react-bootstrap'
+import {CardContext, CartContext} from '../Context/CardContext'
+import Rating from './Rating'
 const Cart = () => {
+    const {state,dispatch}=useContext(CardContext)
     return (
-        <div>
-           cart 
+        <div className="home">
+            <div className="productContainer">
+                <ListGroup>
+                    {state.cart.map(prod=>(
+                        <ListGroup.Item key={prod.id}>
+                            <Row>
+                            <Col md={2}>
+                                    <img src={prod.image} alt={prod.name} />
+                                </Col>
+                                <Col md={2}>
+                                    {prod.name}
+                                </Col>
+                                <Col md={2}>
+                                    {prod.price}
+                                </Col>
+                                <Col md={2}>
+                                    <Rating rating={prod.ratings} />
+                                </Col>
+                                <Col md={2}>
+                                    <Form.Control>
+                                        {[...Array(prod.inStock)].map((x)=>(
+                                            <option key={x+1}>{x+1}</option>
+                                        ))}
+                                    </Form.Control>
+                                </Col>
+                            </Row>
+                        </ListGroup.Item>
+                    ))}
+                </ListGroup>
+
+            </div>
+            <div className="filter">
+
+            </div>
+           
         </div>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,14 @@
 import React,{useContext} from 'react'
-import {Navbar,Container,FormControl,Dropdown,Nav,Badge} from 'react-bootstrap'
+import {Navbar,Container,FormControl,Dropdown,Nav,Badge,Button} from 'react-bootstrap'
 import {FaShoppingCart} from 'react-icons/fa'
 import {Link} from 'react-router-dom'
 import { CardContext } from '../Context/CardContext'
+import {AiFillDelete} from 'react-icons/ai'
 
 
 const Header = () => {
 
-    const {state}=useContext(CardContext)
+    const {state,dispatch}=useContext(CardContext)
     return (
         <Navbar bg="dark" variant="dark" style={{height:80}}>
             <Container>
@@ -32,22 +33,28 @@ const Header = () => {
                        <>
                        {state.cart.map(prod=>(
                            <span key={prod.id} className="cartitem">
-                               <img src={prod.image} alt={prod.name}  />
+                               <img src={prod.image} alt={prod.name} className="cartitemimg"  />
                                <div className="cartitemdetails">
                                    <span>{prod.name}</span>
                                    <span>{prod.price.split('.')[0]}</span>
 
                                </div>
+                               <AiFillDelete 
+                               fontSize='15px'
+                               onClick={()=>{dispatch({type:'REMOVE_FROM_CART',payload:prod})}}
+                               />
 
                            </span>
                        )
 
                        )}
+                       <Link to="/cart"><Button style={{width:'95%'}}>Go to Cart</Button></Link>
 
                        </>
                    ):(
                        <span style={{padding:'10px'}}>Cart isEmpty</span>
                    )}
+
                   
                </Dropdown.Menu>
              </Dropdown>
